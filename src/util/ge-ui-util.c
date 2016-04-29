8d565b9f4bcdaaa878e61b1146c7743834c6939c
Shubham Kumar <shubham.srib@samsung.com>
2016-04-29 18:23:15 +0530

    [Attach-Panel-Gallery]Added Strings in PO
    Change-Id: I0e010a0c181ec40f8b6ee38292d5e847f0521db8
    Signed-off-by: Shubham Kumar <shubham.srib@samsung.com>

--- src/util/ge-ui-util.c
+++ src/util/ge-ui-util.c
@@ -363,9 +363,6 @@ int _ge_ui_set_translate_str(Evas_Object *obj, const char *str)
 	GE_CHECK_VAL(str, -1);
 	GE_CHECK_VAL(obj, -1);
 	char *domain = GE_STR_DOMAIN_LOCAL;
-	if (strstr(str, "IDS_COM")) {
-		domain = GE_STR_DOMAIN_SYS;
-	}
 	elm_object_domain_translatable_text_set(obj, domain, str);
 	return 0;
 }
@@ -378,9 +375,6 @@ int _ge_ui_set_translate_part_str(Evas_Object *obj, const char *part,
 	GE_CHECK_VAL(obj, -1);
 
 	char *domain = GE_STR_DOMAIN_LOCAL;
-	if (strstr(str, "IDS_COM")) {
-		domain = GE_STR_DOMAIN_SYS;
-	}
 
 	elm_object_domain_translatable_part_text_set(obj, part, domain, str);
 
@@ -392,9 +386,6 @@ int _ge_ui_set_translatable_item(Elm_Object_Item *nf_it, const char *str)
 	GE_CHECK_VAL(str, -1);
 	GE_CHECK_VAL(nf_it, -1);
 	char *domain = GE_STR_DOMAIN_LOCAL;
-	if (strstr(str, "IDS_COM")) {
-		domain = GE_STR_DOMAIN_SYS;
-	}
 	elm_object_item_domain_text_translatable_set(nf_it, domain, EINA_TRUE);
 	return 0;
 }
